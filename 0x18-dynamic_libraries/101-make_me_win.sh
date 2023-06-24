402d05f5bc323d9dd362ef9dfd49a07ce7ac19f4
BR-rachid <104208639+BR-rachid@users.noreply.github.com>
2023-06-24 02:21:40 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/SaidBasfaou/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/BR-rachid/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
